8e99538d47e1f63084b70aa30b776a38da513e34
Mario Melo <mario.melo.dev@gmail.com>
2023-10-09 11:19:53 -0500

    Se creo la API con dio

---
+++ lib/src/config/constans.dart
@@ -0,0 +1,3 @@
+const String baseUrl = 'https://dingeltkwmnfigloarcw.supabase.co';
+const String apikey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpbmdlbHRrd21uZmlnbG9hcmN3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5Njg2NDYwOSwiZXhwIjoyMDEyNDQwNjA5fQ.klRm2uY89YV93AaaybEXe5hN6OwLKhcRBMA_MQTx3kw';
---
+++ lib/src/data/api/api.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+
+import 'package:trackstar_web/src/config/constans.dart';
+
+Dio dio = Dio(
+  BaseOptions(
+    baseUrl: baseUrl,
+    validateStatus: (status) {
+      return status! < 500;
+    },
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=utf-8',
+      'apikey': apikey,
+    },
+  ),
+);
