314176f99382609cb76ba5ad624b20b9acc23cd3
Mario Melo <mario.melo.dev@gmail.com>
2023-10-16 12:28:07 -0500

    custom painter

--- lib/src/presentation/screens/categories/productos/widgets/card_info_products.dart
+++ lib/src/presentation/screens/categories/productos/widgets/card_info_products.dart
@@ -9,9 +9,59 @@ class CardInfoProducts extends StatelessWidget {
       width: 300,
       margin: const EdgeInsets.all(15),
       decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.onPrimaryContainer,
+        color: Theme.of(context).colorScheme.primary,
         borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black38,
+            blurRadius: 10,
+            offset: Offset(2, 2),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: CustomPaint(
+          painter: _CustomPaintCard(context: context),
+        ),
       ),
     );
+  }
+}
+
+class _CustomPaintCard extends CustomPainter {
+  final BuildContext context;
+
+  _CustomPaintCard({required this.context});
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = Paint();
+
+    lapiz.color = Theme.of(context).colorScheme.onPrimaryContainer;
+    lapiz.style = PaintingStyle.fill;
+    lapiz.strokeWidth = 5;
+
+    final path = Path();
+
+    //? Crear Header
+    path.lineTo(0, size.height * 0.50);
+    path.quadraticBezierTo(
+        size.width * .25, size.height * .05, size.width, size.height * .10);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    //? Crear Footer
+    path.moveTo(size.width, size.height);
+    path.lineTo(size.width, size.height * 0.8);
+    path.quadraticBezierTo(
+        size.width * 0.95, size.height * 0.95, size.width * 0.7, size.height);
+    path.lineTo(size.width, size.height);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
