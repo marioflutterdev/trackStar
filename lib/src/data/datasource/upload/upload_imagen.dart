093e9af34a52f58b9e2ef7b9ba59099d61e7152f
Mario Melo <mario.melo.dev@gmail.com>
2024-03-01 19:08:38 -0500

    fix_upload_image

--- lib/src/data/datasource/upload/upload_imagen.dart
+++ lib/src/data/datasource/upload/upload_imagen.dart
@@ -1,21 +1,16 @@
 import 'package:image_picker/image_picker.dart';
 import 'package:supabase/supabase.dart';
-import 'dart:math';
 
 import '../../../config/constans/constans.dart';
 
 Future<String> addImage(XFile path) async {
   const supabaseUrl = baseUrl;
   const supabaseKey = apikey;
-  final random = Random();
-  final randomNumber = random.nextInt(90000) + 10000;
   final supabase = SupabaseClient(supabaseUrl, supabaseKey);
   final imageFile = await path.readAsBytes();
   final name = path.name;
 
-  await supabase.storage
-      .from('avatars')
-      .uploadBinary('${name}_$randomNumber', imageFile);
+  await supabase.storage.from('avatars').uploadBinary(name, imageFile);
 
   final urlResponse = supabase.storage.from('avatars').getPublicUrl(name);
 
